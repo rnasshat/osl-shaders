d57652e7ae93a982f2068eafc343a213ba831e69
rna <48500271+rnasshat@users.noreply.github.com>
2025-07-29 23:39:54 -0600

    added complex absolute value and power functions modified mandelbrot_set_texture.osl to have a configurable exponent

--- complex.h
+++ complex.h
@@ -16,3 +16,14 @@ complex __operator__sub__ (complex a, complex b) {
 complex __operator__mul__ (complex a, complex b) {
     return complex(a.real*b.real - a.imag*b.imag, a.real*b.imag + a.imag*b.real);
 }
+
+float cabs (complex c) {
+    return sqrt(c.real*c.real + c.imag*c.imag);
+}
+
+complex pow (complex c, float e) {
+    float phi = atan2(c.imag, c.real);
+    float r = cabs(c);
+    
+    return complex(cos(e*phi) * pow(r, e), sin(e*phi) * pow(r, e));
+}
