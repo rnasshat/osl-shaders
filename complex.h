1e4f24abe7e1d6193697dc02e83ae2765be45d0b
rna <48500271+rnasshat@users.noreply.github.com>
2025-09-29 22:23:48 -0600

    complex exp function; complex power support

--- complex.h
+++ complex.h
@@ -25,12 +25,32 @@ complex __operator__mul__ (float a, complex b) {
     return complex(a, 0) * b;
 }
 
+complex __operator__mul__ (complex a, float b) {
+    return a * complex(b, 0);
+}
+
 float cabs (complex c) {
     return sqrt(c.real*c.real + c.imag*c.imag);
 }
 
 float carg (complex c) {
     return atan2(c.imag, c.real);
+}
+
+complex sin (complex c) {
+    return complex(sin(c.real) * cosh(c.imag), cos(c.real) * sinh(c.imag));
+}
+
+complex cos (complex c) {
+    return complex(cos(c.real) * cosh(c.imag), 0.0 - sin(c.real) * sinh(c.imag));
+}
+
+complex log (complex c) { //principal value
+    return complex(log(cabs(c)), carg(c));
+}
+
+complex exp (complex c) {
+    return complex(exp(c.real) * cos(c.imag), exp(c.real) * sin(c.imag));
 }
 
 complex pow (complex c, float e) {
@@ -40,16 +60,6 @@ complex pow (complex c, float e) {
     return complex(cos(e*phi) * pow(r, e), sin(e*phi) * pow(r, e));
 }
 
-complex log (complex c) {
-    return complex(log(cabs(c)), carg(c));
-}
-
-complex sin (complex c)
-{
-    return complex(sin(c.real) * cosh(c.imag), cos(c.real) * sinh(c.imag));
-}
-
-complex cos (complex c)
-{
-    return complex(cos(c.real) * cosh(c.imag), 0.0 - sin(c.real) * sinh(c.imag));
+complex pow (complex c, complex e) {
+    return exp(e * log(c));
 }
